8dc76a0ad1ac9f04b7867c204abf72d2562989b3
dubung <101760142+dubung@users.noreply.github.com>
2025-07-17 21:29:47 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+
+	int T,a,b;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++) {
+		for (int j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
