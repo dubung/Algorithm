5b9e92dbb8a138b4927648ebec9f013230751c65
dubung <101760142+dubung@users.noreply.github.com>
2025-07-23 20:10:35 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int checkReplaceCnt(char ** arr,int h, int w);
+int main() {
+
+   
+    char chess[50][50];
+
+    int h, w;
+
+    scanf("%d%d ", &h, &w);
+
+    for (int H = 0; H < h; H++) {
+       scanf("%s", chess[H]);
+        chess[H][w] = '\0';
+    }
+    int result = 640;
+    int iCnt = 0, iCnt2 = 0;
+    char chB = 'B';
+    char chW = 'W';
+
+    for (int H = 0; H <= h - 8; H++) {
+        for (int W = 0; W <= w - 8; W++) {
+            
+            for (int t = 0; t <= 2; t++) {
+                iCnt = 0;
+                for (int checkH = 0; checkH < 8; checkH++) {
+                    for (int checkW = 0; checkW < 4; checkW++) {
+
+                        if (t == 0) {
+                            if (checkH % 2 == 0) {
+
+                                if (chess[H + checkH][W + 2 * checkW] != chB) {
+                                    iCnt++;
+                                }if (chess[H + checkH][W + 2 * checkW + 1] != chW) {
+                                    iCnt++;
+                                }
+                            }
+                            else {
+                                if ( chess[H + checkH][W + 2 * checkW] != chW) {
+                                    iCnt++;
+                                }if (chess[H + checkH][W + 2 * checkW + 1] != chB) {
+                                    iCnt++;
+                                }
+                            }
+                        }
+                        else {
+                            if (checkH % 2 == 0) {
+                                if (chess[H + checkH][W + 2 * checkW] != chW) {
+                                    iCnt++;
+                                }if (chess[H + checkH][W + 2 * checkW + 1] != chB) {
+                                    iCnt++;
+                                }
+                            }
+                            else {
+
+                                if (chess[H + checkH][W + 2 * checkW] != chB) {
+                                    iCnt++;
+                                }if (chess[H + checkH][W + 2 * checkW + 1] != chW) {
+                                    iCnt++;
+                                }
+                            }
+
+                        }
+                    }
+                }
+                if (result > iCnt)
+                    result = iCnt;
+            }
+
+        }
+    }
+
+
+    
+    printf("%d",result);
+    return 0;
+}
