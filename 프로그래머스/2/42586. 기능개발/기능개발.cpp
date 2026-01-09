a8a00476ed7a48af907be5ff203897800b151411
dubung <101760142+dubung@users.noreply.github.com>
2026-01-09 12:04:19 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -4,35 +4,33 @@
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-     vector<int> answer;
+    vector<int> answer;
 
   
- queue<int> answerQueue;
+    queue<int> answerQueue;
 
- for (int i = 0; i < progresses.size(); i++) {
-     answerQueue.push(progresses[i]);
- }
+    for (int i = 0; i < progresses.size(); i++) { // 큐에 넣어줌
+        answerQueue.push(progresses[i]);
+    }  
 
- int iDay = 1;
- int iCnt = 0;
- while (1){
+    int iDay = 1; // 며칠 흘렀을까요
+    int iCnt = 0; // 해당일에 끝낸 작업을 나타낼 변수
+    while (1){
 
-     if (answerQueue.size() != 0 &&(iDay * (speeds[progresses.size() - answerQueue.size()]) + answerQueue.front() > 99)) {
-         answerQueue.pop();
-         iCnt++;
+    if (answerQueue.size() != 0 && (iDay * (speeds[progresses.size() - answerQueue.size()]) + answerQueue.front() > 99)) { // 일수 * 1일당작업량이 99보다 크다면
+         answerQueue.pop(); //작업 큐 제거
+         iCnt++; 
          continue;
      }
-     if (iCnt)
+     if (iCnt) // 완료된 작업이 있다면 작업된 갯수 push
          answer.push_back(iCnt);
      iCnt = 0;
      iDay++;
 
-     if (answerQueue.empty())
+     if (answerQueue.empty())// 작업 큐가 비지않았다면
          break;
- }
+    }
 
- if (!answerQueue.empty())
-     answer.push_back(iCnt);
     
      return answer;
 }
